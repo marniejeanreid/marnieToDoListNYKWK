dc7d1f01bcb75f424be46ba64be75085a41f5abb
Marnie Reid <marniejeanreid@gmail.com>
2022-07-28 10:27:45 -0700

    Complete Iteration 0

---
+++ marnieToDoList/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  marnieToDoList
+//
+//  Created by Scholar on 7/28/22.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    var important = false
+    
+    
+}
